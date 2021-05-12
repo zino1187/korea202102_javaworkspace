6841f17f0bc22db6c4a2c8c42df389cb9266bfab
zino1187 <zino1187@gmail.com>
2021-05-12 17:28:43 +0900

    c

--- app0512/src/app0512/graphic/Gallery.java
+++ app0512/src/app0512/graphic/Gallery.java
@@ -4,12 +4,15 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class Gallery extends JFrame{
+public class Gallery extends JFrame implements ActionListener{
 	//커스터마이징 할 필요가 있으니, JPanel의 코드를 수정하자
 	GalleryDetail galleryDetail; //상세이미지 그려질 패널
 	
@@ -22,6 +25,7 @@ public class Gallery extends JFrame{
 	String[] arr= {"1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg"};//파일명
 	Toolkit kit;
 	Image[] image=new Image[arr.length]; //배열생성
+	int index=3; //이미지 배열의 접근용 index 
 	
 	public Gallery() {
 		//생성
@@ -37,7 +41,7 @@ public class Gallery extends JFrame{
 		
 		//이미지가 생성되었으므로, 0번째 즉 첫번째 이미지를
 		//디폴트로 그려지게 하자!!
-		galleryDetail.setImage(image[0]);
+		galleryDetail.setImage(image[index]);
 		
 		
 		//레이아웃, 스타일 
@@ -49,11 +53,37 @@ public class Gallery extends JFrame{
 		p_south.add(bt_next);
 		add(p_south, BorderLayout.SOUTH);
 		
+		bt_prev.addActionListener(this);//이전버튼과 리스너 연결 
+		bt_next.addActionListener(this);//다음버튼과 리스너 연결
+		
 		//보여주기
 		setBounds(1600, 100,500, 400);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
+	
+	public void actionPerformed(ActionEvent e) {
+		//하나의  actoinPerformed메서드에서 여러 컴포넌트즉 이벤트 소스를 구분해야 한다 
+		if(e.getSource() == bt_prev) {
+			if(index>0) {
+				index--;
+			}else {
+				JOptionPane.showMessageDialog(this, "이전 이미지가 없습니다");
+			}
+		}else if(e.getSource() == bt_next) {
+			if(index<arr.length-1) {
+				index++; //그 다음 이미지를 접근하기 위해 index 1 증가
+			}else {
+				JOptionPane.showMessageDialog(this, "다음 이미지가 없습니다");
+			}
+		}
+		System.out.println("index="+index);
+		
+		//상세보기 객체에 변경된 이미지 보내주기 
+		galleryDetail.setImage(image[index]);
+		galleryDetail.repaint();//다시 그리기 요청
+	}
+	
 	public static void main(String[] args) {
 		new Gallery();
 	}
