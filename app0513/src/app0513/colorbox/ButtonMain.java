092665b3f96399cdc5a31d10cfc231a7893319c4
zino1187 <zino1187@gmail.com>
2021-05-13 11:51:36 +0900

    c

--- app0513/src/app0513/colorbox/ButtonMain.java
+++ app0513/src/app0513/colorbox/ButtonMain.java
@@ -18,6 +18,9 @@ public class ButtonMain extends JFrame implements ActionListener{
 	JButton bt_bg;
 	JPanel p_center;
 	
+	//대부분의 응용프로그램에서는 배열 선언시 반드시 그 크기를 명시해야 한다..
+	JButton[] btnArray;
+	
 	public ButtonMain() {
 		//생성 
 		p_north = new JPanel();
@@ -50,17 +53,22 @@ public class ButtonMain extends JFrame implements ActionListener{
 	public void create() {
 		//(1) 텍스트필의 값 구하기
 		int ea = Integer.parseInt(t_ea.getText()); // "" 벗겨내기
+		btnArray=new JButton[ea];//배열의 크기를 결정지을 수 있다..따라서 이때 배열을 생성할 수 있다...
 		
 		//(2) 개수만큼 버튼을 생성하여  p_center에 붙이기!!
 		for(int i=0;i<ea;i++) {
 			JButton bt= new JButton("버튼"+i);
 			p_center.add(bt);
+			btnArray[i] = bt; //배열에, 지금 생성된 버튼을 탑재!!
 		}
 		p_center.updateUI();//다시 그려라
 	}
 	
 	public void setBg() {
-		
+		//배열에 들어있는 버튼 수 만큼 배경색 적용 
+		for(int i=0;i<btnArray.length;i++) {
+			btnArray[i].setBackground(Color.RED);
+		}
 	}
 	
 	public void actionPerformed(ActionEvent e) {
