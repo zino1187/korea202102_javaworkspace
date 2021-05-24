0f2a1e7f96c0ccebfad0c04cb2e27a9c20e11918
zino1187 <zino1187@gmail.com>
2021-05-24 10:27:01 +0900

    c

---
+++ ShoppingApp/src/com/minssam/shoppingapp/util/FileManager.java
@@ -0,0 +1,33 @@
+package com.minssam.shoppingapp.util;
+
+//앞으로, 파일과 관련된 여러 작업을 전담하게 될 파일제어 클래스 정의 
+public class FileManager {
+	
+	//넘겨받은 경로를 통해 확장자만 추출해보기 
+	public static String getExtend(String path) {
+		int lastIndex = path.lastIndexOf("\\"); //넘겨받은 문자열내의 가장 마지막 디렉토리구분자의 위치
+		//System.out.println(lastIndex);
+		//String str = "korea";
+		//System.out.println(str.subSequence(0, str.length()));
+		String filename = path.substring(lastIndex+1, path.length());
+		String ext = filename.substring(filename.lastIndexOf(".")+1, filename.length());
+		
+		return ext;
+	}
+	
+	public static void main(String[] args) {
+		String result = getExtend("D:\\workspace\\korea202102_jsworkspace\\images\\dog.jpg");
+		System.out.println(result);
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
