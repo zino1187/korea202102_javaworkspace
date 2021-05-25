20cfbb2af9d0ff58fef2cb2b4f184fc705bddcac
zino1187 <zino1187@gmail.com>
2021-05-25 14:34:40 +0900

    c

--- ShoppingApp/src/com/minssam/shoppingapp/main/AppMain.java
+++ ShoppingApp/src/com/minssam/shoppingapp/main/AppMain.java
@@ -13,6 +13,7 @@ import java.sql.SQLException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import com.minssam.shoppingapp.config.ConfigMain;
@@ -38,6 +39,7 @@ public class AppMain extends JFrame implements ActionListener{
 	String user="root";
 	String password="1234";
 	private Connection con;
+	private boolean session=false; //세션이 true 일때 인증받은 것이고, false일때는 미인증 간주
 	
 	public AppMain() {
 		connect();//DB접속
@@ -87,6 +89,13 @@ public class AppMain extends JFrame implements ActionListener{
 		}
 		
 		//보여주기
+		//인증여부에 따라 알맞는 페이지 보여주기 
+		if(session==false) { //인증을 받지 않은 상태이므로, 로그인을 디폴트로 보여주자!!
+			showHide(4);//제일 먼저 보여주고 싶은 페이지
+		}else {
+			showHide(0);
+		}
+		
 		setBounds(0, 10, 1200, 700);
 		setVisible(true);
 		
@@ -99,16 +108,11 @@ public class AppMain extends JFrame implements ActionListener{
 		//있기에, 즉 가진게 별로 없기에 버튼의 특징을 이용하기 위해서는 버튼 형으로 변환해서 사용하자!!
 		CustomButton bt=(CustomButton)obj; //down casting
 		//System.out.println(bt.getText());
-		
-		//내가 누른 버튼에 해당하는 페이지만 setVisible() 을 true로 놓고 나머지는 false로 놓자!!
-		for(int i=0;i<pages.length;i++) {
-			if(bt.getId()==i) {
-				pages[i].setVisible(true); //현재 선택한 버튼과 같은 인덱스를 갖는 페이지라면..
-			}else {
-				pages[i].setVisible(false);
-			}
+		if(session) {
+			showHide(bt.getId());
+		}else {
+			JOptionPane.showMessageDialog(this , "로그인이 필요한 서비스입니다");
 		}
-		
 	}
 	
 	public void connect() {
@@ -172,6 +176,17 @@ public class AppMain extends JFrame implements ActionListener{
 	
 	public Connection getCon() {
 		return con;
+	}
+	
+	public void showHide(int n) { //보여주고 싶은 페이지의 번호를 넘기면 된다..
+		//내가 누른 버튼에 해당하는 페이지만 setVisible() 을 true로 놓고 나머지는 false로 놓자!!
+		for(int i=0;i<pages.length;i++) {
+			if(n==i) {
+				pages[i].setVisible(true); //현재 선택한 버튼과 같은 인덱스를 갖는 페이지라면..
+			}else {
+				pages[i].setVisible(false);
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
--- ShoppingApp/src/com/minssam/shoppingapp/member/LoginForm.java
+++ ShoppingApp/src/com/minssam/shoppingapp/member/LoginForm.java
@@ -1,13 +1,73 @@
 package com.minssam.shoppingapp.member;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 import com.minssam.shoppingapp.main.AppMain;
 import com.minssam.shoppingapp.main.Page;
 
 public class LoginForm extends Page{
+	JPanel p_container;//BorderLayout 
+	JPanel p_center; //form 
+	JPanel p_south; //버튼 영역 
+	JLabel la_id, la_pass;
+	JTextField t_id; 
+	JPasswordField t_pass;
+	JButton bt_login, bt_join;
+	
 	public LoginForm(AppMain appMain) {
 		super(appMain);
-		setBackground(Color.CYAN);
+		
+		//생성
+		p_container = new JPanel();
+		p_center = new JPanel();
+		p_south = new JPanel();
+		la_id = new JLabel("ID");
+		la_pass = new JLabel("Password");
+		t_id = new JTextField();
+		t_pass = new JPasswordField();
+		bt_login = new JButton("Login");
+		bt_join = new JButton("회원가입");
+		
+		//스타일 레이아웃
+		p_container.setPreferredSize(new Dimension(250, 85));
+		p_container.setLayout(new BorderLayout());
+		p_center.setLayout(new GridLayout(2, 2));
+		
+		//조립
+		p_center.add(la_id);
+		p_center.add(t_id);
+		p_center.add(la_pass);
+		p_center.add(t_pass);
+		
+		p_south.add(bt_login);
+		p_south.add(bt_join);
+		
+		p_container.add(p_center);
+		p_container.add(p_south, BorderLayout.SOUTH);
+		
+		add(p_container);
+		
+		//리스너
+		//보이기
 	}
 }
+
+
+
+
+
+
+
+
+
+
