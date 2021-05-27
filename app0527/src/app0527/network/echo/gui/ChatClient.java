830d9dc4a837cb152c136b0cf06ae91934aafbed
zino1187 <zino1187@gmail.com>
2021-05-27 13:22:01 +0900

    c

---
+++ app0527/src/app0527/network/echo/gui/ChatClient.java
@@ -0,0 +1,61 @@
+package app0527.network.echo.gui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class ChatClient extends JFrame{
+	JPanel p_north;
+	JTextField t_ip;
+	JTextField t_port;
+	JButton bt_connect;
+	JTextArea area;
+	JScrollPane scroll;
+	JPanel p_south;
+	JTextField t_input;
+	JButton bt_send;
+	
+	public ChatClient() {
+		p_north = new JPanel();
+		t_ip = new JTextField(10);
+		t_port = new JTextField(5);
+		bt_connect = new JButton("접속");
+		area = new JTextArea();
+		scroll = new JScrollPane(area);
+		p_south = new JPanel();
+		t_input = new JTextField(20);
+		bt_send = new JButton("send");
+		
+		//조립 
+		p_north.add(t_ip);
+		p_north.add(t_port);
+		p_north.add(bt_connect);
+		add(p_north, BorderLayout.NORTH);
+		add(scroll);
+		p_south.add(t_input);
+		p_south.add(bt_send);
+		add(p_south, BorderLayout.SOUTH);
+		
+		//이벤트 
+		
+		//보이기 
+		setVisible(true);
+		setBounds(2200, 100, 300, 400);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args) {
+		new ChatClient();
+
+	}
+
+}
+
+
+
+
