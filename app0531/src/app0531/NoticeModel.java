95d2d3fa52e5c208752de243917d3b26984c78ae
zino1187 <zino1187@gmail.com>
2021-05-31 13:20:35 +0900

    c

---
+++ app0531/src/app0531/NoticeModel.java
@@ -0,0 +1,33 @@
+package app0531;
+
+import java.util.Vector;
+
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.AbstractTableModel;
+
+//notice 게시판 전용 TableModel 
+public class NoticeModel extends AbstractTableModel{
+	Vector<Notice> data=new Vector<Notice>(); //기존의 데이터를 처리했던 이차원배열을 대신할 컬렉션 객체
+	Vector<String> column=new Vector<String>(); //기존의 컬럼명을 처리했던 일차원배열을 대신할 컬렉션 객체
+	
+	public int getRowCount() {
+		System.out.println("getRowCount is "+data.size());
+		return data.size(); //VO가 담겨진 벡터의 크기
+	}
+	public int getColumnCount() {
+		System.out.println("컬럼의 수는 "+column.size());
+		return column.size();
+	}
+	
+	//이 메서드는 getColumnCount() 의 반환값만큼 호출하면서 매개변수 col의 값을 0부터 증가시키며 호출. 
+	public String getColumnName(int col) {//0,1,2,3....순서대로 col을 증가시키며 호출
+		return column.get(col);  
+	}
+	public Object getValueAt(int row, int col) {
+		System.out.println("getValueAt("+row+", "+col+")");
+		return "apple";
+	}
+	
+
+}
