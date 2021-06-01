dbac5ed879ff095c3d04068f13010f67b62a28fd
zino1187 <zino1187@gmail.com>
2021-06-01 16:29:23 +0900

    c

--- app0601/src/app0601/json/chat/LoginForm.java
+++ app0601/src/app0601/json/chat/LoginForm.java
@@ -1,11 +1,14 @@
 package app0601.json.chat;
 
 import java.awt.BorderLayout;
-import java.awt.Button;
 import java.awt.Dimension;
-import java.awt.Label;
-import java.awt.Panel;
-import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -26,6 +29,12 @@ public class LoginForm extends JFrame{
 	
 	JPanel p_center;
 	JPanel p_south;
+	
+	//db관련 
+	Connection con;
+	String url="jdbc:mysql://localhost:3306/javase?characterEncoding=UTF-8";
+	String user="root";
+	String password="1234";
 	
 	//이 윈도우인 즉 로그인폼이 메모리에 생성될때, 해당 부품들도 같이 생성시키려면
 	//생성자 메서드를 적극 활용해야 한다..
@@ -61,13 +70,55 @@ public class LoginForm extends JFrame{
 		this.add(p_center);
 		this.add(p_south, BorderLayout.SOUTH);
 		
-		//현재 클래스가 Frame을 상속받은 자식이므로, Frame이 보유한 
-		//메서드를 내것처럼 쓸 수 있다..
+		//이벤트 연결 
+		bt_login.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				loginCheck();
+			}
+		});
+		
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				release(con);
+				System.exit(0);//kill process 
+				
+			}
+		});
+		
 		this.setSize(350,155);//윈도우의 크기 설정 
 		this.setVisible(true);//윈도우 보이게
 		
-		//윈도우에 제목달기 - 방법1 부모의 일반메서드 이용  
-		//this.setTitle("LogionForm style 2");
+		connect();
+	}
+	
+	public void connect() {
+		/*
+		 1) 드라이버 로드 
+		 2) 접속 
+		 */
+		try {
+			Class.forName("com.mysql.jdbc.Driver"); //드라이버 로드 
+			con = DriverManager.getConnection(url, user, password);
+			if(con!=null) {
+				this.setTitle("접속 성공");
+			}else {
+				this.setTitle("접속 실패");
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void release(Connection con) {
+		if(con !=null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
