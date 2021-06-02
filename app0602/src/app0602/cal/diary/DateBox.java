66f7a77cb2d2d111e522176110e2cf46b431963b
zino1187 <zino1187@gmail.com>
2021-06-02 14:47:28 +0900

    c

--- app0602/src/app0602/cal/diary/DateBox.java
+++ app0602/src/app0602/cal/diary/DateBox.java
@@ -11,16 +11,18 @@ public class DateBox extends JPanel{
 	int width;
 	int height;
 	String day;
+	Color color; //배경색 
 	
 	public DateBox(String day,Color color, int width, int height) {
 		this.day=day;
+		this.color=color;
 		this.width=width;
 		this.height=height;
 		setPreferredSize(new Dimension(width, height));
 	}
 
 	public void paint(Graphics g) {
-		g.setColor(Color.GRAY);//그래픽 객체의 물감이 회색이다!!
+		g.setColor(color);//그래픽 객체의 물감색을 결정한다
 		g.fillRect(0, 0, width, height);//채워진 사각형
 
 		g.setColor(Color.WHITE);//물감을 흰색으로 교체
--- app0602/src/app0602/cal/diary/Diary.java
+++ app0602/src/app0602/cal/diary/Diary.java
@@ -2,6 +2,7 @@ package app0602.cal.diary;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.util.Calendar;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -32,7 +33,12 @@ public class Diary extends JFrame{
 		p_north.add(bt_next);
 		add(p_north, BorderLayout.NORTH);
 		add(p_center);
-		createDay();
+		
+		createDay(); //요일생성
+		createDate();//날짜생성
+		
+		//테스트
+		System.out.println(getFirstDayOfMonth(2021, 1-1)); //1월 
 		
 		//이벤트 
 		
@@ -53,6 +59,28 @@ public class Diary extends JFrame{
 	}
 	
 	//날짜 생성 
+	public void createDate() {
+		for(int i=0;i<dayArray.length*6;i++) {
+			DateBox dateBox = new DateBox(Integer.toString(i), Color.orange, 100,100);
+			p_center.add(dateBox); //센터에 부착!!
+		}
+	}
+	
+	//해당 월의 시작 요일 구하기!!
+	//개발원리: 날짜 객체를 해당월의 1일로 조작한 후, 요일을 구한다!!
+	//단 해당월과 연도는 매개변수로 호출자가 그 값을 결정할 수 있도록 메서드 정의하자
+	public int getFirstDayOfMonth(int yy, int mm) {
+		Calendar cal=Calendar.getInstance(); //날짜 객체 생성 
+		cal.set(yy,mm, 1);//해당년도와 월의 1을로 조작!!
+		return cal.get(Calendar.DAY_OF_WEEK)-1; //주의 js 에서의 습관이 있기에 요일은 0부터 시작하는 걸로..		
+	}
+	
+	//해당월이 몇일까지 있는지 구하기!!  8월 31일, 9월 30일
+	public int getLastDate() {
+		Calendar cal = Calendar.getInstance();
+		
+		return 0;
+	}
 	
 	public static void main(String[] args) {
 		new Diary();
